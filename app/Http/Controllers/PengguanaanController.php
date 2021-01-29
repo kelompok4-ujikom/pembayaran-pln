6d89aa0dff01fd048415f3b4e50b77ec4a9f5942
AhmadF1129 <medf20062003@gmail.com>
2021-01-29 16:11:09 +0700

    Menambahkan Migration

---
+++ app/Http/Controllers/PengguanaanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pengguanaan;
+use Illuminate\Http\Request;
+
+class PengguanaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pengguanaan  $pengguanaan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengguanaan $pengguanaan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pengguanaan  $pengguanaan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pengguanaan $pengguanaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pengguanaan  $pengguanaan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengguanaan $pengguanaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pengguanaan  $pengguanaan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengguanaan $pengguanaan)
+    {
+        //
+    }
+}
