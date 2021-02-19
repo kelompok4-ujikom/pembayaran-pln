2d354933fd1a16ebc9bdf673440af201f0952b5b
Zulfikar08 <zulfikarsubang89@gmail.com>
2021-02-19 18:19:44 +0700

    test commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('levels')->insert([
+            'name' => 'admin',
+        ]);
+        DB::table('levels')->insert([
+            'name' => 'pelanggan',
+        ]);
+        DB::table('levels')->insert([
+            'name' => 'bank',
+        ]);
+    }
+}
